93da636af87776ba0c163564c18d1cc358ce6ad8
Valiantsin Anikin <valiantsin.anikin@bayer.com>
2023-06-20 18:34:31 +0200

    task-5

--- import-service/lib/import-service.importFileParser.ts
+++ import-service/lib/import-service.importFileParser.ts
@@ -1,70 +1,30 @@
 import csvParser from 'csv-parser';
 import { S3Event } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
-const s3 = new AWS.S3();
 
-export const handler = async (
-  event: S3Event,
-  _: any,
-  callback: (err: any, data: any) => void
-) => {
-  const results: any[] = [];
+export const handler = async (event: S3Event) => {
+  const promises = event.Records.map((record) => {
+    const bucketName = record?.s3?.bucket?.name || '';
+    const objectKey = record?.s3?.object?.key || '';
 
-  try {
-    console.log(`Event: ${event}`);
+    const params = { Bucket: bucketName, Key: objectKey };
+    const stream = new AWS.S3().getObject(params).createReadStream();
 
-    for (const record of event.Records) {
-      const bucketName = record?.s3?.bucket?.name || '';
-      const objectKey = record?.s3?.object?.key || '';
-
-      const params = { Bucket: bucketName, Key: objectKey };
-      s3.getObject(params)
-        .createReadStream()
-        // .pipe(csvParser())
+    return new Promise(function (resolve, reject) {
+      stream
+        .pipe(csvParser())
         .on('data', (data) => {
-          console.log('data: ', data);
-          results.push(data);
+          console.log('data: ', JSON.stringify(data));
         })
         .on('error', function (err) {
           console.error('Got an error: ' + err);
+          reject(err);
         })
-        .on('end', () => {
-          console.log('results:', results);
-          // callback(null, {
-          //   statusCode: 200,
-          //   headers: {
-          //     'Access-Control-Allow-Headers': 'Content-Type',
-          //     'Access-Control-Allow-Origin': '*',
-          //     'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-          //   },
-          //   body: JSON.stringify({ results }),
-          // });
-          console.log('done:', results);
+        .on('end', (rows: any) => {
+          console.log(`Parsed ${rows} rows`);
+          resolve(rows);
         });
-      console.log(`results: ${results}`);
-
-      // const data = response.Body?.toString('utf-8') || '';
-      // // result += data;
-      // console.log('record:', record);
-      // console.log('data:', data);
-    }
-
-    // return {
-    //   statusCode: 200,
-    //   headers: {
-    //     'Access-Control-Allow-Headers': 'Content-Type',
-    //     'Access-Control-Allow-Origin': '*',
-    //     'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-    //   },
-    //   body: JSON.stringify({ results1: results }),
-    // };
-  } catch (err) {
-    console.log(err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'some error happened',
-      }),
-    };
-  }
-};
+    });
+  });
+  return Promise.all(promises);
+}
